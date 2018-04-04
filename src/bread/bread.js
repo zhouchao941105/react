953d80378bf180511b9e153c62137f738684e121
zhouchao <zhouchao1105@gmail.com>
2018-04-04 22:08:58 +0800

    q

--- src/bread/bread.js
+++ src/bread/bread.js
@@ -8,7 +8,7 @@ const Apps = () => (
       <Link to="/apps/1">Application1</Link>：<Link to="/apps/1/detail">Detail</Link>
     </li>
     <li>
-      <Link to="/apps/2">Application2</Link>：<Link to="/apps/2/detail">Detail</Link>
+      <Link to="/apps/2">Application</Link>：<Link to="/apps/2/detail">Detail</Link>
     </li>
   </ul>
 );
