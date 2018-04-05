1f68c18244538288b96688ff6766160121447faa
zhouchao <zhouchao1105@gmail.com>
2018-04-06 00:20:52 +0800

    react-router

--- src/index.js
+++ src/index.js
@@ -1,61 +1,65 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  { HashRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom'
-import { DatePicker, Button, From, Layout, Icon, Menu,Breadcrumb } from 'antd'
+import { HashRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom'
+import { DatePicker, Button, From, Layout, Icon, Menu, Breadcrumb } from 'antd'
 import moment from 'moment'
 import './index.css';
 // import 'antd/dist/antd.less';
 const { RangePicker } = DatePicker
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
-import Home from './bread/bread'
-class Chat extends React.Component {
-     App=()=>(
-        <ul>
-            <li>
-                <Link to="">x</Link>
-                <Link to="">y</Link>
-            </li>
-            <li>
-            <Link to="">z</Link>
-            <Link to="">r</Link>
-            </li>
-        </ul>
-)
+import Home, { Apps } from './bread/bread'
+class App extends React.Component {
+    // App = () => (
+    //     <ul>
+    //         <li>
+    //             <Link to="">x</Link>
+    //             <Link to="">y</Link>
+    //         </li>
+    //         <li>
+    //             <Link to="">z</Link>
+    //             <Link to="">r</Link>
+    //         </li>
+    //     </ul>
+    // )
     render() {
         return (
             <Layout>
                 <Sider>
-                    <Menu theme='dark' mode='inline'>
+
+                    <Router>
+                        <Apps></Apps>
+                    </Router>
+                    {/* <Menu theme='dark' mode='inline'>
                         <Menu.Item key='1'>
-                        <Icon type="pie-chart" />
-                        <Router>
-                            <Link to="/">Home</Link>
-                        </Router>
-                        </Menu.Item>
-                        
-                        {/* <SubMenu title='hehe'>
+                            <Icon type="pie-chart" />
+                            <Router>
+                                <Link to="/" style={{ display: 'inline-block' }}>Home</Link>
+                            </Router>
+                        </Menu.Item>*/}
+
+                    {/* <SubMenu title='hehe'>
                             <Menu.Item>
                                 haha
                             </Menu.Item>
                         </SubMenu> */}
-                        <Menu.Item key="2">
+                    {/* <Menu.Item key="2">
                             <Icon type="desktop" />
                             <Router>
-                                <Link to="/apps">Apps</Link>
-                        </Router>
-                                
+                                <Link to="/apps" style={{ display: 'inline-block' }}>Apps</Link>
+                            </Router>
                         </Menu.Item>
-                        
-                    </Menu>
+                    </Menu> */}
                 </Sider>
                 <Layout>
                     <Router>
-                        <Home></Home>
+                        <Route>
+                            <Home></Home>
+                        </Route>
                     </Router>
-                    <Header style={{ background: '#fff', padding: 0 ,color:'pink'}}>
-                        chat online
+                    {/* <Header style={{ background: '#fff', padding: 0, color: 'pink' }}>
+                        App online
                     </Header>
                     <Content>
                         <div style={{ padding: 24, background: '#ffd', minHeight: 360 }}>
@@ -64,7 +68,7 @@ class Chat extends React.Component {
                     </Content>
                     <Footer style={{ textAlign: 'center' }}>
                         SIS
-                    </Footer>
+                    </Footer> */}
                 </Layout>
             </Layout>
         )
@@ -74,10 +78,10 @@ class Chat extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Chat></Chat>
+    <App></App>
     ,
     document.getElementById('root')
 );
-if(module.hot){
+if (module.hot) {
     module.hot.accept()
 }
