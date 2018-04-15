16b7ec62b94a9371ea8cde55ddd9d16c72dba3ec
zhouchao <zhouchao1105@gmail.com>
2018-04-15 17:40:25 +0800

    使用axios与mongoose完成数据通信

--- src/tab/index.js
+++ src/tab/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Tabs } from 'antd'
+import axios from 'axios'
 const TabPane = Tabs.TabPane;
 class TabList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: null
+        }
+    }
+    getList = (key) => {
+        axios.get('http://127.0.0.1:9090/get', {
+            params: {
+                type: key
+            }
+        }).then(data => {
+            this.setState({
+                data: data.data.map(item => item.name)
+            })
+        })
     }
     render() {
         return (
-            <Tabs>
+            <Tabs onChange={this.getList}>
                 {this.props.list.map((item, idx) => {
                     return (
-                        <TabPane tab={`tab${item}`} key={idx}>{item}</TabPane>
+                        <TabPane tab={`tab${item}`} key={idx}>{this.state.data || item}</TabPane>
                     )
                 })}
             </Tabs>
